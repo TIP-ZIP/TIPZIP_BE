c484713dd42ae20f48675097956b02389b6aaeb2
byunhm02 <byunhm02@gmail.com>
2024-11-12 11:56:05 +0900

    Chore: 프로젝트 생성

---
+++ src/main/java/mutsa/TIPZIP_BE/TipzipBeApplication.java
@@ -0,0 +1,13 @@
+package mutsa.TIPZIP_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipzipBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipzipBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mutsa/TIPZIP_BE/TipzipBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mutsa.TIPZIP_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipzipBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
