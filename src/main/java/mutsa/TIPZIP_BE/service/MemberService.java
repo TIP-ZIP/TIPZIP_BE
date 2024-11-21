4dea88550b3a513d2a1cdcec9b3a1938bd4142f2
byunhm02 <byunhm02@gmail.com>
2024-11-21 17:07:41 +0900

    feat(service): getMemberFromKakao메서드 정의(#2)

--- src/main/java/mutsa/TIPZIP_BE/service/MemberService.java
+++ src/main/java/mutsa/TIPZIP_BE/service/MemberService.java
@@ -19,6 +19,7 @@ public class MemberService {
     // jpa, mysql dependency 추가
     private final MemberRepository memberRepository;
     private static final String KAKAO_USERINFO_URL = "https://kapi.kakao.com/v2/user/me";
+    private static final String GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo";
     private final JwtTokenProvider jwtTokenProvider;
 
     public void save(MemberDTO memberDTO) {
@@ -74,6 +75,64 @@ public class MemberService {
             }
         }else {
             System.out.println("카카오에서 사용자 정보를 가져오지 못했습니다.");
+        }
+        memberDTO.setNewMember(isNewMember);
+
+        //JWT토큰 발급
+        String jwtAccessToken=jwtTokenProvider.createToken(memberDTO.getEmail(),3600);
+        String jwtRefreshToken=jwtTokenProvider.createToken(memberDTO.getEmail(),86400);
+        memberDTO.setAccessToken(jwtAccessToken);
+        memberDTO.setRefreshToken(jwtRefreshToken);
+        System.out.println("jwtAccessToken: " + jwtAccessToken);
+        System.out.println("jwtRefreshToken: " + jwtRefreshToken);
+        return memberDTO;
+
+    }
+    public MemberDTO getMemberFromGoogle(String accessToken) {
+        System.out.println("이제 구글 서버에서 유저 정보를 가져오겠습니다.");
+        //Webclient사용해 카카오 api 호출
+        WebClient webClient = WebClient.create();
+        String response = webClient.get()
+                .uri(GOOGLE_USERINFO_URL)
+                .header("Authorization", "Bearer " + accessToken)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        // JSON 응답 파싱
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(response);
+        JsonObject googleAccount = element.getAsJsonObject();
+        // 필요한 데이터 추출
+        String nickname = googleAccount.get("name").getAsString();
+        System.out.println("구글에서 사용중인 닉네임: " + nickname);
+        String email= googleAccount.get("email").getAsString();
+        System.out.println("email: " + email);
+
+        // MemberDTO 생성 및 설정
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setSocial_id(nickname); // Social_id를 MemberDTO에 설정
+        memberDTO.setEmail(email);
+
+        boolean isNewMember=false;
+        // 카카오로부터 받은 정보가 있을 경우 추가 설정
+        if (memberDTO.getSocial_id() != null) {
+            // 사용자 정보 확인을 위한 출력
+            System.out.println("구글에서 가져온 사용자 정보:");
+            System.out.println("Member Name: " + memberDTO.getSocial_id());
+            System.out.println("Member Email: " + memberDTO.getEmail()); // 예시로 이메일이 있다고 가정
+
+            MemberEntity existingMember = memberRepository.findByEmail(memberDTO.getEmail()).orElse(null);
+            if (existingMember == null) {
+                // 신규 회원일 경우 저장
+                isNewMember=true;
+                System.out.println("신규회원입니다. 멤버를 저장하고싶어요..ㅠㅠ");
+                save(memberDTO); //// DTO를 저장하는 메서드 호출
+            }else{
+                System.out.println("이미 존재하는 회원입니다.:"+existingMember.getSocial_id());
+            }
+        }else {
+            System.out.println("구글에서 사용자 정보를 가져오지 못했습니다.");
         }
         memberDTO.setNewMember(isNewMember);
 
