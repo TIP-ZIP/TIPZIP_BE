02d9733e1662194c402ecc1f121466b25a51da7b
byunhm02 <byunhm02@gmail.com>
2024-11-22 10:13:22 +0900

    fix: security관련 설정 추가(#2)

--- src/main/java/mutsa/TIPZIP_BE/TipzipBeApplication.java
+++ src/main/java/mutsa/TIPZIP_BE/TipzipBeApplication.java
@@ -2,8 +2,9 @@ package mutsa.TIPZIP_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TipzipBeApplication {
 
 	public static void main(String[] args) {
