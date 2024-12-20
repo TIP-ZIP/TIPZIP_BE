f1f57d4d9c13d055f28c5ca5a9ae6a4d286cf91d
byunhm02 <byunhm02@gmail.com>
2024-12-20 16:28:04 +0900

    feat(controller): 팔로잉,팔로워 수 반환 api 구현(#27)

--- src/main/java/mutsa/TIPZIP_BE/controller/FollowController.java
+++ src/main/java/mutsa/TIPZIP_BE/controller/FollowController.java
@@ -8,10 +8,10 @@ import mutsa.TIPZIP_BE.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/follow")
@@ -35,6 +35,26 @@ public class FollowController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
         }
     }
+    @GetMapping
+    public ResponseEntity<?> getFollowInfo(@RequestHeader("Authorization")String token) {
+        try{
+            MemberEntity member=memberRepository.findById(memberService.getUserFromToken(token).getUser_id())
+                    .orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾을 수 없습니다"));
+            int followingCount= followService.getFollowingCount(member);
+            int followerCount= followService.getFollowerCount(member);
+            System.out.println("내가 팔로우한 사람 수: " + followingCount);
+            System.out.println("나를 팔로우한 사람 수: " + followerCount);
+            Map<String,Integer> response=new HashMap<>();
+            response.put("followingCount",followingCount);
+            response.put("followerCount",followerCount);
+            return ResponseEntity.ok(response);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+        }
+    }
+
 }
 
 
