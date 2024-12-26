5c91a14ac9688826615208234adacc85c7c60f93
ttr0bin <monica990131@gmail.com>
2024-12-26 22:56:28 +0900

    feat(Post): add getFollowingPostsList (#51)

--- src/main/java/mutsa/TIPZIP_BE/repository/FollowRepository.java
+++ src/main/java/mutsa/TIPZIP_BE/repository/FollowRepository.java
@@ -23,6 +23,8 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     */
     int countByFollower(MemberEntity follower);
     int countByFollowing(MemberEntity following);
+
+    List<MemberEntity> findByFollower_id(Long id);
 }
 
 
