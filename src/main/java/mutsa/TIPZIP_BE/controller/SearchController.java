414d22cbd4283dbf57333f750db9861db3053553
byunhm02 <byunhm02@gmail.com>
2024-12-26 23:35:02 +0900

    fix(controller):태그 기반 검색 수정(#44)

--- src/main/java/mutsa/TIPZIP_BE/controller/SearchController.java
+++ src/main/java/mutsa/TIPZIP_BE/controller/SearchController.java
@@ -7,10 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/search")
@@ -20,10 +23,16 @@ public class SearchController {
     @GetMapping
     public ResponseEntity<?> searchPosts(
             @RequestParam(value = "search", required = false) String searchKeyword,
-            @RequestParam(value = "tags", required = false) List<String> tags) {
+            @RequestParam(value = "tags",required = false) String tags) {
+        List<String> tagList = (tags != null && !tags.isEmpty())
+                ? Arrays.asList(tags.split(","))
+                : null;
         //검색 결과 가져오기
         try{
-            List<PostResponseDTO> results=searchService.searchPosts(searchKeyword,tags);
+            // Request Body에서 태그 리스트 추출
+            //List<String> tags = (body != null) ? body.get("tags") : null;
+            //List<PostResponseDTO> results=searchService.searchPosts(searchKeyword,tags);
+            List<PostResponseDTO> results = searchService.searchPosts(searchKeyword, tagList);
             return ResponseEntity.ok(results);
         }catch (IllegalArgumentException e){
             return ResponseEntity.badRequest().body(e.getMessage());
