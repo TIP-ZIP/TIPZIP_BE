a0f49aea2b1346aad02b8068d16deb5230fabba1
ttr0bin <monica990131@gmail.com>
2024-12-29 03:01:04 +0900

    fix(Post): fix ResponseDTO to contain user_id, category, profile_image (#75)

--- src/main/java/mutsa/TIPZIP_BE/dto/PostDTO/PostResponseDTO.java
+++ src/main/java/mutsa/TIPZIP_BE/dto/PostDTO/PostResponseDTO.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 // 상세보기
-public record PostResponseDTO(Long id, String author, boolean badge, List<String> tag, String createdAt, String title, String content, boolean scrap, long scrapCount, String link_url, String thumbnail_url) {
+public record PostResponseDTO(Long id, String author, Long user_id, String profile_image, boolean badge, List<String> tag, String createdAt, String title, String category, String content, boolean scrap, long scrapCount, String link_url, String thumbnail_url) {
 
     public PostResponseDTO(Post post) {
-        this(post.getId(), post.getMemberEntity().getUsername(),post.getMemberEntity().getBadge(), post.getPostTags(),
-                post.getCreatedAt().toString(), post.getTitle(), post.getContent(),
-                post.isScrapped(), post.getScrapCount(), post.getLink_url(), post.getThumbnail_url());
+        this(post.getId(), post.getMemberEntity().getUsername(), post.getMemberEntity().getUserId(), post.getMemberEntity().getProfile_image(),
+                post.getMemberEntity().getBadge(), post.getPostTags(),post.getCreatedAt().toString(), post.getTitle(), post.getCategory().getCategoryName(),
+                post.getContent(),post.isScrapped(), post.getScrapCount(), post.getLink_url(), post.getThumbnail_url());
     }
 }
--- src/main/java/mutsa/TIPZIP_BE/dto/PostDTO/PostSimpleDTO.java
+++ src/main/java/mutsa/TIPZIP_BE/dto/PostDTO/PostSimpleDTO.java
@@ -3,10 +3,10 @@ package mutsa.TIPZIP_BE.dto.PostDTO;
 import mutsa.TIPZIP_BE.entity.Post;
 
 // 미리보기 형식으로 내보내는 경우
-public record PostSimpleDTO(Long id, String author, boolean badge, String title, boolean scrap, long scrapCount, String thumbnail_url) {
+public record PostSimpleDTO(Long id, String author, Long user_id, String profile_image, boolean badge, String title, String category, boolean scrap, long scrapCount, String thumbnail_url) {
 
     public PostSimpleDTO(Post post) {
-        this(post.getId(), post.getMemberEntity().getUsername(), post.getMemberEntity().getBadge(),
-                post.getTitle(), post.isScrapped(), post.getScrapCount(), post.getThumbnail_url());
+        this(post.getId(), post.getMemberEntity().getUsername(), post.getMemberEntity().getUserId(), post.getMemberEntity().getProfile_image(),
+                post.getMemberEntity().getBadge(), post.getTitle(), post.getCategory().getCategoryName(), post.isScrapped(), post.getScrapCount(), post.getThumbnail_url());
     }
 }
