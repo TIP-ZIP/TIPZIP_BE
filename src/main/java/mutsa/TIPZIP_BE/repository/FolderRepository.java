90e5775ae1879b70bbfa6f6e19211cc4d42cb989
ttr0bin <monica990131@gmail.com>
2024-12-29 23:34:51 +0900

    fix(Folder): contain member (#89)

--- src/main/java/mutsa/TIPZIP_BE/repository/FolderRepository.java
+++ src/main/java/mutsa/TIPZIP_BE/repository/FolderRepository.java
@@ -1,14 +1,17 @@
 package mutsa.TIPZIP_BE.repository;
 
 import mutsa.TIPZIP_BE.entity.Folder;
+import mutsa.TIPZIP_BE.entity.MemberEntity;
 import mutsa.TIPZIP_BE.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
 
     Optional<Folder> findByFolderName(String name);
+    List<Folder> findByMemberEntity(MemberEntity memberEntity);
 }
